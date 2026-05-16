27cb38abc03876e648eb8cccda7736059cdfe757
Raghvendra9402 <singhraghvendra405@gmail.com>
2026-05-16 14:32:03 +0530

    Add GitHub Actions workflow for EC2 app updates and update homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl">
-        Welcome to the Deployment of an nextjs app in ec2 instance
+      <h1 className="text-4xl text-rose-500 font-bold">
+        Like, Share and Subscribe and watch out my other playlist also
       </h1>
-      <span>Have fun!</span>
+      <span>I hope you liked this class ❤️</span>
     </div>
   );
 }
